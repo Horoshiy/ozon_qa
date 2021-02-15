ffba5f2a77cf78a6b84d34a3d8747b8726cbb2f4
Horoshiy <info@dinamchiki.ru>
2021-02-15 09:01:45 +0300

    console and topBar checks

--- src/test/java/ru/ozon/tests/MainPageTests.java
+++ src/test/java/ru/ozon/tests/MainPageTests.java
@@ -1,6 +1,7 @@
 package ru.ozon.tests;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
@@ -8,8 +9,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static ru.ozon.helpers.DriverHelper.getConsoleLogs;
 
 @Tag("web")
 @Feature("Main Page content")
@@ -22,5 +30,21 @@ public class MainPageTests extends TestBase {
     void ozonPictureMainPageTest() {
         open("");
         $("header img").shouldHave(Condition.attribute("alt", "Ozon"));
+    }
+
+    @Test
+    @Story("Check for loading and errors")
+    @AllureId("1653")
+    @DisplayName("Console log should not have any errors")
+    void consoleLogShouldNotHaveErrorsTest() {
+        open("");
+        assertThat(getConsoleLogs(), not(containsString("SEVERE")));
+    }
+
+    @Test
+    void topBarCheckTest() {
+        open("");
+        $(by("data-widget", "topBar")).find("button").should(exist);
+        $(by("data-widget", "topBar")).shouldHave(text("Мобильное приложение"));
     }
 }
